da71e7e34340bbea9ad56a8074cc8cf4ecf0a86e
unknown <HP@RIHLF101892.bjsc.local>
2026-03-18 14:46:10 +0300

    name in google

--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -1,7 +1,18 @@
+// مراجعة: جعل الدالة تدعم المسارات المحلية والروابط الخارجية معاً
+export const getImg = (pathOrId, sz = "w1500") => {
+  if (!pathOrId) return "";
+
+  // إذا كان المسار يبدأ بـ / معناه صورة محلية في مجلد public
+  if (pathOrId.startsWith('/')) {
+    return pathOrId;
+  }
+  
+  // غير ذلك، يفترض أنه ID من جوجل درايف (كخيار احتياطي)
+  return `https://drive.google.com/thumbnail?id=${pathOrId}&sz=${sz}`;
+};
+
+// الثوابت الأخرى تبقى كما هي
 export const API_URL = "https://semak.sa/api.php";
-
-export const getImg = (id, sz = "w1500") => `https://drive.google.com/thumbnail?id=${id}&sz=${sz}`;
-
 export const TIME_SLOTS = [
   "08:00 ص - 10:00 ص",
   "10:00 ص - 12:00 م",
